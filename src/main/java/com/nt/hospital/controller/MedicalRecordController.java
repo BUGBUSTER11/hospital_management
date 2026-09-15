6a04e6146412d6faf17595f1b43c32992adf5e1b
BUGBUSTER11 <inamdaraisha03@gmail.com>
2026-09-15 14:11:39 +0530

    Controller for medical record

---
+++ src/main/java/com/nt/hospital/controller/MedicalRecordController.java
@@ -0,0 +1,8 @@
+package com.nt.hospital.controller;
+
+public class MedicalRecordController {
+
+    public void addMedicalRecord() {
+        System.out.println("Record added successfully....");
+    }
+}

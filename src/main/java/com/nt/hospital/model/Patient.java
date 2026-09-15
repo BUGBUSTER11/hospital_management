523c74b816061d1e04ed5a0179e5c6bfcda8a7d8
Shivtejbhukan <shivatejbhukan955@gmail.com>
2026-09-15 23:10:24 +0530

    Create Two Entity Classes Doctor.java and Patient.java in model Package

---
+++ src/main/java/com/nt/hospital/model/Patient.java
@@ -0,0 +1,154 @@
+package com.nt.hospital.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public class Patient {
+    @Id
+    private int patientId;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private LocalDate dateOfBirth;
+    private String bloodGroup;
+    private String phone;
+    private String email;
+    private String address;
+    private String emergencyContactName;
+    private String emergencyContactPhone;
+    private LocalDate registrationDate;
+
+    public Patient(){}
+
+    public Patient(LocalDate dateOfBirth, int patientId, String firstName, String lastName, String gender, String bloodGroup, String phone, String email, String address, String emergencyContactName, String emergencyContactPhone, LocalDate registrationDate) {
+        this.dateOfBirth = dateOfBirth;
+        this.patientId = patientId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.bloodGroup = bloodGroup;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+        this.emergencyContactName = emergencyContactName;
+        this.emergencyContactPhone = emergencyContactPhone;
+        this.registrationDate = registrationDate;
+    }
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getBloodGroup() {
+        return bloodGroup;
+    }
+
+    public void setBloodGroup(String bloodGroup) {
+        this.bloodGroup = bloodGroup;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmergencyContactName() {
+        return emergencyContactName;
+    }
+
+    public void setEmergencyContactName(String emergencyContactName) {
+        this.emergencyContactName = emergencyContactName;
+    }
+
+    public String getEmergencyContactPhone() {
+        return emergencyContactPhone;
+    }
+
+    public void setEmergencyContactPhone(String emergencyContactPhone) {
+        this.emergencyContactPhone = emergencyContactPhone;
+    }
+
+    public LocalDate getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDate registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "patientId=" + patientId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", gender='" + gender + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", bloodGroup='" + bloodGroup + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                ", emergencyContactName='" + emergencyContactName + '\'' +
+                ", emergencyContactPhone='" + emergencyContactPhone + '\'' +
+                ", registrationDate=" + registrationDate +
+                '}';
+    }
+}

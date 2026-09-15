1fc5228851b2f3d1e72efecded7b434835c81d31
ShubhamFartade <shubhamfartade7070@example.com>
2026-09-15 14:26:29 +0530

    this is my prescriptve file

---
+++ src/main/java/com/nt/hospital/Prescription.java
@@ -0,0 +1,11 @@
+package com.nt.hospital;
+
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
+public class Prescription {
+
+    public void prescription() {
+        System.out.println("Record added successfully....");
+    }
+
+}

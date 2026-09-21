4303345c6acb9451d6d585cc9c8584dd7512eefc
BUGBUSTER11 <inamdaraisha03@gmail.com>
2026-09-21 13:45:13 +0530

    User flow

---
+++ src/main/java/com/nt/hospital/service/DepartmentService.java
@@ -0,0 +1,9 @@
+package com.nt.hospital.service;
+
+public interface DepartmentService {
+
+    public void addDepartment();
+//    public void removeDepartment();
+//    public void showDepartment();
+
+}
---
+++ src/main/java/com/nt/hospital/service/DepartmentServiceImpl.java
@@ -0,0 +1,10 @@
+package com.nt.hospital.service;
+
+public class DepartmentServiceImpl implements DepartmentService {
+
+
+    @Override
+    public void addDepartment() {
+
+    }
+}
